1f44caaa6aefe716389aaf68bb151f53995d0497
Hassan <hassan.aurangzaib@code-engine.net>
2025-11-01 01:30:19 +0500

    saving

---
+++ client/src/Pages/Users/CreateClient.jsx
@@ -0,0 +1,270 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createClient } from "../../redux/action/user";
+import { useNavigate } from "react-router-dom";
+import Topbar from "./Topbar";
+import {
+  Divider,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  DialogActions,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { PiNotepad, PiXLight } from "react-icons/pi";
+import { CFormSelect } from "@coreui/react";
+import { pakistanCities } from "../../constant";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+const CreateClientComponent = ({ open, setOpen, scroll }) => {
+  //////////////////////////////////////// VARIABLES /////////////////////////////////////
+  const { isFetching } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const initialClientState = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    phone: "",
+    email: "",
+  }
+
+  //////////////////////////////////////// STATES /////////////////////////////////////
+  const [clientData, setClientData] = useState(initialClientState);
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    phone: "",
+    email: "",
+  });
+
+  //////////////////////////////////////// USE EFFECTS /////////////////////////////////////
+
+  //////////////////////////////////////// FUNCTIONS /////////////////////////////////////
+  const validateForm = () => {
+    const newErrors = {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      phone: "",
+      email: "",
+    };
+    let isValid = true;
+
+    if (!clientData.firstName.trim()) {
+      newErrors.firstName = "First Name is required";
+      isValid = false;
+    }
+
+    if (!clientData.lastName.trim()) {
+      newErrors.lastName = "Last Name is required";
+      isValid = false;
+    }
+
+    if (!clientData.username.trim()) {
+      newErrors.username = "Username is required";
+      isValid = false;
+    }
+
+    if (!clientData.password.trim()) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    } else if (clientData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+      isValid = false;
+    }
+
+    if (!clientData.phone.trim()) {
+      newErrors.phone = "Phone is required";
+      isValid = false;
+    }
+
+    if (clientData.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(clientData.email)) {
+      newErrors.email = "Please enter a valid email address";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+    dispatch(createClient(clientData, setOpen));
+    setClientData(initialClientState);
+    setErrors({
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      phone: "",
+      email: "",
+    });
+  };
+
+  const handleChange = (field, value) => {
+    setClientData((prevFilters) => ({ ...prevFilters, [field]: value }));
+    // Clear error when user starts typing
+    if (errors[field]) {
+      setErrors((prevErrors) => ({ ...prevErrors, [field]: "" }));
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setClientData(initialClientState);
+    setErrors({
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      phone: "",
+      email: "",
+    });
+  };
+
+  return (
+    <div>
+      <Dialog
+        scroll={scroll}
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        fullWidth="sm"
+        maxWidth="sm"
+        aria-describedby="alert-dialog-slide-description">
+        <DialogTitle className="flex items-center justify-between">
+          <div className="text-sky-400 font-primary">Add New Client</div>
+          <div className="cursor-pointer" onClick={handleClose}>
+            <PiXLight className="text-[25px]" />
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <div className="flex flex-col gap-2 p-3 text-gray-500 font-primary">
+            <div className="text-xl flex justify-start items-center gap-2 font-normal">
+              <PiNotepad size={23} />
+              <span>Client Details</span>
+            </div>
+            <Divider />
+            <table className="mt-4">
+              <tr>
+                <td className="pb-4 text-lg">First Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={clientData.firstName}
+                    onChange={(e) => handleChange('firstName', e.target.value)}
+                    error={!!errors.firstName}
+                    helperText={errors.firstName}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">Last Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={clientData.lastName}
+                    onChange={(e) => handleChange('lastName', e.target.value)}
+                    error={!!errors.lastName}
+                    helperText={errors.lastName}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">User Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={clientData.username}
+                    onChange={(e) => handleChange('username', e.target.value)}
+                    error={!!errors.username}
+                    helperText={errors.username}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">Email </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Optional"
+                    value={clientData.email}
+                    onChange={(e) => handleChange('email', e.target.value)}
+                    error={!!errors.email}
+                    helperText={errors.email}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="flex items-start pt-2 text-lg">Password </td>
+                <td className="pb-4">
+                  <TextField
+                    type="password"
+                    value={clientData.password}
+                    onChange={(e) => handleChange("password", e.target.value)}
+                    size="small"
+                    fullWidth
+                    error={!!errors.password}
+                    helperText={errors.password}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="flex items-start pt-2 text-lg">Phone </td>
+                <td className="pb-4">
+                  <TextField
+                    type="number"
+                    size="small"
+                    value={clientData.phone}
+                    onChange={(e) => handleChange("phone", e.target.value)}
+                    fullWidth
+                    error={!!errors.phone}
+                    helperText={errors.phone}
+                  />
+                </td>
+              </tr>
+            </table>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <button
+            onClick={handleClose}
+            variant="contained"
+            type="reset"
+            className="bg-[#d7d7d7] px-4 py-2 rounded-lg text-gray-500 mt-4 hover:text-white hover:bg-[#6c757d] border-[2px] border-[#efeeee] hover:border-[#d7d7d7] font-thin transition-all">
+            Cancel
+          </button>
+          <button
+            onClick={handleSubmit}
+            variant="contained"
+            className="bg-primary-red px-4 py-2 rounded-lg text-white mt-4 hover:bg-red-400 font-thin">
+            {isFetching ? 'Submitting...' : 'Submit'}
+          </button>
+        </DialogActions>
+      </Dialog>
+    </div>
+
+  );
+};
+
+export default CreateClientComponent;
+
